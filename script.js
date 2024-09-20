8e2bcbbd689260e3fcf2a21eea1c11d22f25c189
Tommyisready <perrot.thomas@live.fr>
2024-09-20 15:38:27 +0200

    site final terminé

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ navCloseBtn.addEventListener("click", () => {
 window.addEventListener("scroll", () => {
   // get the current vertical scroll position
   const scrollY = window.scrollY;
-  console.log(scrollY);
 
   // add or remove the active class from the header bases on the scroll position
   scrollY > 50 ? header.classList.add("is-active") : header.classList.remove("is-active");
@@ -54,3 +53,92 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+//faq
+
+const faqItems = document.querySelectorAll(".faq__item");
+
+faqItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const isOpen = item.classList.toggle("is-open");
+    const iconClass = isOpen ? "ri-arrow-up-s-line" : "ri-arrow-down-s-line";
+    const iconElement = item.querySelector("i");
+    iconElement.classList = `${iconClass}`;
+  });
+});
+
+// scroll reveal
+const sr = ScrollReveal({
+  origin: "bottom",
+  distance: "60px",
+  duration: 3000,
+  delay: 600,
+  reset: true,
+});
+
+// custom configurations for specific elements
+
+const revealFromTop = {
+  origin: "top",
+};
+
+const revealWithInterval = {
+  distance: "100px",
+  interval: 100,
+};
+
+const revealFromLeft = {
+  origin: "left",
+  distance: "100px",
+};
+
+const revealFromRight = {
+  origin: "right",
+  distance: "100px",
+};
+
+const revealWithShortDistance = {
+  distance: "60px",
+};
+const revealWithDelay = {
+  distance: "100px",
+  delay: 400,
+};
+
+//banner
+sr.reveal(".banner__text", revealFromTop);
+sr.reveal(".banner__image");
+
+//brands
+sr.reveal(".brands__title", revealWithDelay);
+sr.reveal(".brands__img", revealWithInterval);
+
+//benefits
+sr.reveal(".benefits__title", revealWithShortDistance);
+sr.reveal(".benefit__text", revealFromLeft);
+sr.reveal(".benefit__img", revealFromRight);
+sr.reveal(".benefit--2 .benefit__img", revealFromLeft);
+sr.reveal(".benefit--2 .benefit__img", revealFromRight);
+
+//testimonials
+sr.reveal(".testimonials, .testimonials__container", {
+  distance: "100px",
+});
+
+//services
+sr.reveal(".services__title, .services__item", revealWithInterval);
+
+//FAQ
+sr.reveal(".faq__img", {
+  distance: "100px",
+  origin: "top",
+  interval: 100,
+});
+sr.reveal(".faq__title, .faq__item", revealWithInterval);
+
+// contact
+sr.reveal(".contact__form-wrapper");
+sr.reveal(".contact__img", revealFromTop);
+
+// footer
+sr.reveal(".footer");
